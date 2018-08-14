9c3804c16f5105b583ef0251a5386470a15a27a0
Cleverson Parreira Júnior <parreirajr@gmail.com>
2018-08-13 23:12:24 -0300

    Repassando informações das views para os controllers: RequestParam

--- SpringMVC/src/br/com/parreira/springmvc/controllers/HelloController.java
+++ SpringMVC/src/br/com/parreira/springmvc/controllers/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -30,6 +31,12 @@ public class HelloController {
 	
 	@RequestMapping("/receberMensagem/{mensagem}")
 	public String receberMensagem(Model model, @PathVariable("mensagem") String msg) {
+		model.addAttribute("mensagem", msg);
+		return "mensagemDoServidor";
+	}
+	
+	@RequestMapping("/receberMensagem2")
+	public String receberMensagem2(Model model, @RequestParam(value = "mensagem", required = false, defaultValue = "Mensagem padrão") String msg) {
 		model.addAttribute("mensagem", msg);
 		return "mensagemDoServidor";
 	}
