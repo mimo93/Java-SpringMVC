6a8c257c2b41141bede164fd71f66e6297cc55dd
Cleverson Parreira Júnior <parreirajr@gmail.com>
2018-08-25 21:03:15 -0300

    Spring Data: fazendo a integração com o controller

--- SpringMVC/src/br/com/parreira/springmvc/controllers/AlbunsController.java
+++ SpringMVC/src/br/com/parreira/springmvc/controllers/AlbunsController.java
@@ -1,20 +1,35 @@
 package br.com.parreira.springmvc.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.com.parreira.springmvc.dominios.Album;
+import br.com.parreira.springmvc.repositorios.RepositorioAlbum;
 
 @Controller
 @RequestMapping("/albuns")
 public class AlbunsController {
 
+	@Autowired
+	private RepositorioAlbum repositorio;
+	
+	@RequestMapping(value = "/listar", method = RequestMethod.GET)
+	public String listar(Model model) {
+		List<Album> albuns = repositorio.findAll();
+		model.addAttribute("albuns", albuns);
+		return "album.listar.tiles";
+	}
+	
 	@RequestMapping(value= "/adicionar", method = RequestMethod.GET)
 	public String adicionar(Model model) {
 		model.addAttribute("album", new Album());
@@ -26,8 +41,30 @@ public class AlbunsController {
 		if (result.hasErrors()) {
 			return "album.adicionar.tiles";
 		}
-		model.addAttribute("album", novoAlbum);
-		return "album.exibir.tiles";
+		repositorio.save(novoAlbum);
+		return "redirect:/albuns/listar";
+	}
+	
+	@RequestMapping(value = "/alterar/{id}", method = RequestMethod.GET)
+	public String alterar(@PathVariable("id") Long id, Model model) {
+		Album albumASerAlterado = repositorio.findOne(id);
+		model.addAttribute("album", albumASerAlterado);
+		return "album.alterar.tiles";
+	}
+	
+	@RequestMapping(value = "/alterar", method = RequestMethod.POST)
+	public String alterar(@ModelAttribute("album") @Valid Album album, BindingResult result) {
+		if (result.hasErrors()) {
+			return "album.alterar.tiles";
+		}
+		repositorio.save(album);
+		return "redirect:/albuns/listar";
+	}
+	
+	@RequestMapping(value = "/excluir", method = RequestMethod.GET)
+	public String excluir(@PathVariable("id") Long id) {
+		repositorio.delete(id);
+		return "redirect:/albuns/listar";
 	}
 	
 }
